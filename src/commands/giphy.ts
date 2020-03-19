3e2ee2baa8c4534668566b9292feed243070cf09
Jaden Buchan <me@jaden.bio>
2020-03-19 03:08:41 +0000

    Add Basic Giphy Command

--- src/commands/giphy.ts
+++ src/commands/giphy.ts
@@ -1,22 +1,34 @@
 import { CommandUtils } from './index';
+import axios, { AxiosResponse } from 'axios';
+
+interface GiphyResponse {
+	data: {
+		type: string;
+		url: string;
+		embed_url: string;
+	};
+}
 
 export async function giphy(tag: string, { printLine }: CommandUtils): Promise<void> {
 	if(!tag || tag.length === 0) {
 		tag = 'fail';
 	}
 
-	const res = await fetch('https://api.giphy.com/v1/gifs/random', {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify({
+	axios.get('https://api.giphy.com/v1/gifs/random', {
+		params: {
 			// eslint-disable-next-line @typescript-eslint/camelcase
 			api_key: 'vw3qPqDfxllhPzBSrl6veNUiy08foPrY',
 			rating: 'pg-13',
 			tag,
-		}),
-	})
-		.then((res) => res.json())
-		.catch(() => null);
+		},
+	}).then((res: AxiosResponse<GiphyResponse>) => {
+		if(!res.data) {
+			alert('UH OH!');
+			return;
+		}
 
-	console.log(res);
+		window.open(res.data.data.embed_url, '_new');
+	});
+
+	// console.log(res);
 }
